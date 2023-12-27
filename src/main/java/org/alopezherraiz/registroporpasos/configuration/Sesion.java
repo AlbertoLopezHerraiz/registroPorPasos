03643c051574efff45a6bf78576861e8609efd52
Alberto Lopez Herraiz <alopezherraiz@educa.madrid.org>
2023-12-27 19:17:39 +0100

    27/12/23 19.17

--- src/main/java/org/alopezherraiz/registroporpasos/configuration/Sesion.java
+++ src/main/java/org/alopezherraiz/registroporpasos/configuration/Sesion.java
@@ -4,9 +4,10 @@ import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDate;
+
 @Getter @Setter @ToString
 @NoArgsConstructor @AllArgsConstructor
-public class DatosPersonales {
+public class Sesion {
     @NotNull
     private String nombre;
     private String apellidos;
--- src/main/java/org/alopezherraiz/registroporpasos/controller/Controlador.java
+++ src/main/java/org/alopezherraiz/registroporpasos/controller/Controlador.java
@@ -1,15 +1,18 @@
 package org.alopezherraiz.registroporpasos.controller;
 
+import ch.qos.logback.core.model.Model;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 import java.util.Map;
 
-import static org.alopezherraiz.registroporpasos.configuration.Colecciones.getGeneros;
+import static org.alopezherraiz.registroporpasos.configuration.Colecciones.*;
 
 @Controller
 public class Controlador {
@@ -17,10 +20,9 @@ public class Controlador {
     private Map<String, String> devuelveListaGeneros(){
         return getGeneros();
     }
-    @GetMapping("datos1")
-    public String datosPersonales(Model modelo,
-                                  @RequestParam String usuario,
-                                  HttpSession sesion){
-        return "datos-personales";
+    @RequestMapping("datos1")
+    public String datosPersonales(Model modelo, @Valid @ModelAttribute("sesion") HttpSession sesion){
+
+        return "datospersonales";
     }
 }
