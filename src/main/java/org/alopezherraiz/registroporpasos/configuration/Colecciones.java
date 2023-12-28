fae7397ef9e90b501467b4763571ab1ba9b40609
Alberto Lopez Herraiz <alopezherraiz@educa.madrid.org>
2023-12-28 13:40:53 +0100

    28/12/23 13.40

--- src/main/java/org/alopezherraiz/registroporpasos/configuration/Colecciones.java
+++ src/main/java/org/alopezherraiz/registroporpasos/configuration/Colecciones.java
@@ -5,13 +5,29 @@ import java.util.Map;
 
 public class Colecciones {
     private static final Map<String, String> GENEROS= new HashMap<>();
+    private static final Map<String, String> EXTRAS= new HashMap<>();
+    private static final Map<String, String> NACIONALIDAD= new HashMap<>();
 
     static {
         GENEROS.put("F", "Femenino");
         GENEROS.put("M", "Masculino");
-        GENEROS.put("O", "Otro");}
+        GENEROS.put("O", "Otro");
+        EXTRAS.put("C", "Casado o Pareja de hecho");
+        EXTRAS.put("H", "Hijos");
+        NACIONALIDAD.put("E", "Española");
+        NACIONALIDAD.put("F", "Francesa");
+        NACIONALIDAD.put("I", "Italiana");
+        NACIONALIDAD.put("P", "Portuguesa");
+    }
 
     public static Map<String, String> getGeneros(){
         return GENEROS;
     }
+    public static Map<String, String> getExtras(){
+        return EXTRAS;
+    }
+    public static Map<String, String> getNacionalidades(){
+        return NACIONALIDAD;
+    }
+
 }
--- src/main/java/org/alopezherraiz/registroporpasos/controller/Controlador.java
+++ src/main/java/org/alopezherraiz/registroporpasos/controller/Controlador.java
@@ -4,11 +4,12 @@ import ch.qos.logback.core.model.Model;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 
+import org.alopezherraiz.registroporpasos.configuration.Sesion;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Map;
 
@@ -20,9 +21,26 @@ public class Controlador {
     private Map<String, String> devuelveListaGeneros(){
         return getGeneros();
     }
-    @RequestMapping("datos1")
-    public String datosPersonales(Model modelo, @Valid @ModelAttribute("sesion") HttpSession sesion){
+    @ModelAttribute("extras")
+    private Map<String, String> devuelveListaExtras(){
+        return getExtras();
+    }
+    @ModelAttribute("nacionalidades")
+    private Map<String, String> devuelveListaNacionalidades(){
+        return getNacionalidades();
+    }
 
+    @GetMapping("datos1")
+    public String datosPersonales(Sesion datos, HttpSession sesion){
+        return "datospersonales";
+    }
+    @PostMapping("datos1")
+    public String datosPersonalesPost(Sesion datos, HttpSession sesion){
+       // sesion.setAttribute("apellidos", datos.setApellidos(sesion.getAttribute("apellidos")));
+        return "redirect:/datos1";
+    }
+    @GetMapping("datos2")
+    public String datosProfesionales(Sesion datos, HttpSession sesion){
         return "datospersonales";
     }
 }
